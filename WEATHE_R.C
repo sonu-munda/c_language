55727cd5a70adddee1e1b397a698c37703169245
sonu-munda <sonumunda333@gmail.com>
2022-11-01 09:46:18 +0530

    Create WEATHE_R.C

---
+++ WEATHE_R.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a,c,d=40;
+printf("Your weather in degree C");
+scanf("%d",&a);
+ c=d-a;
+ printf("It can be hotter if the\nwheather goes more %d degree C.",c);
+ getch();
+ }
